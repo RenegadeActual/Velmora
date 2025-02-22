69b110b42d0967feb2fcb0eeced7fe674d32661d
William Whatley <wwwhatley@icloud.com>
2025-02-21 21:11:45 -0600

    partial commit #2

--- engine/src/game_types.h
+++ engine/src/game_types.h
@@ -6,3 +6,19 @@
  * Represents the basic game state in a game.
  * Called for creation by the application
  */
+typedef struct game {
+    // The application configuration.
+    application_config app_config;
+
+    //function pointer to game's initialize fuction.
+    b8 (*initialize)(struct game* game_inst);
+
+    // Function pointer to game's update function
+    b8 (*update)(struct game* game_inst, f32 delta_time);
+
+    // Function pointer to game's render function.
+    b8 (*render)(struct game* game_inst, f32 delta_time);
+
+    // Function pointer to handle resizes, if applicable.
+    // Left off on #006 22:24 Application Layer and Entry point
+}
